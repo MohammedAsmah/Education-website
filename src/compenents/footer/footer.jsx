06b2ba1f817341e3ae869267e51148e9642dcb6e
Simo Asmah <simoasmah@gmail.com>
2024-12-31 10:05:12 +0100

    some changes on the footer to make it responsive

--- src/compenents/footer/footer.jsx
+++ src/compenents/footer/footer.jsx
@@ -2,7 +2,7 @@ import './footer.css'
 export default function Footer(){
 
     return(
-        <div className="container">
+        <div className="foot-container">
         <div className="footer" >
             <h3>© 2025 Educity.All rights reserved by <span className='myname' >Asmah</span> </h3>
             <div>
